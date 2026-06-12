2a8f7a3b667ed9ed85692f93c9acf40a53874aa4
asiftemur-yamin <asiftemur@gmail.com>
2026-06-12 04:45:10 -0700

    Update firebaseConfig.ts

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,1 +1,16 @@
+// firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  // Yahan aapki wohi purani keys aayengi
+  apiKey: "AIza...", 
+  authDomain: "scrap-world-app.firebaseapp.com",
+  projectId: "scrap-world-app",
+  storageBucket: "scrap-world-app.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
