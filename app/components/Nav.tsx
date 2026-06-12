a636feced09998a2564b6b945e9a4512da2f922e
asiftemur-yamin <asiftemur@gmail.com>
2026-06-12 06:13:06 -0700

    Update Nav.tsx

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -7,7 +7,8 @@ export default function Nav() {
       <Link href="/" className="text-xs font-black text-slate-600">🏠 Home</Link>
       <Link href="/my-ads" className="text-xs font-black text-slate-600">📦 My Ads</Link>
       <Link href="/post-ad" className="text-xs font-black text-indigo-600 border border-indigo-600 px-3 py-1 rounded-full">+ Post</Link>
-      <Link href="/auth" className="text-xs font-black text-slate-600">👤 Login</Link>
+      {/* Ye line change ki hai taake ye current page pe hi param change kare */}
+      <Link href="/?page=login" className="text-xs font-black text-slate-600">👤 Login</Link>
     </nav>
   );
 }
